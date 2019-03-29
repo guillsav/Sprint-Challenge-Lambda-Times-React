2dbc0816696318eff055685c6c28c754392360e7
guillsav <guillsav@gmail.com>
2019-03-29 13:58:48 -0400

    destructed the selectedHandler props on the Tab component

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Tab = props => {
-  const {tab, selectedTab} = props;
+  const {tab, selectedTab, selectTabHandler} = props;
+
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
@@ -12,7 +13,7 @@ const Tab = props => {
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-        props.selectTabHandler(tab);
+        selectTabHandler(tab);
       }}
     >
       {tab.toUpperCase()}
